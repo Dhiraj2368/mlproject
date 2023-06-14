11e1e12269e81cce43ba37314a05545d1ffacd98
Dhiraj2368 <dhirajkumar30101998@gmail.com>
2023-06-14 22:51:52 +0530

    Added new files

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 def error_message_detail(error,error_details:sys):
     _,_,exe_tb=error_details.exe_info()
@@ -17,3 +17,5 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
     
+
+
